a3ba033301c6bb9764e25cf1f4cdcd980edd49e0
alexizator <akilesh0876@gmail.com>
2022-07-27 09:15:38 +0530

    Additional Pages

--- app.py
+++ app.py
@@ -1,15 +1,53 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hellofromtheotherside'
+
 
 @app.route('/')
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM POSTS').fetchall()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('index.html',posts=posts)
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection
+            conn.execute('INSERT INTO posts (title,content) VALUES (?,?)',(title,content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
