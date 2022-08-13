a4f3be0d4c341aa736e7c6d4eca767be019b32a8
alexizator <akilesh0876@gmail.com>
2022-08-13 12:11:22 +0530

    hello

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+from email import contentmanager
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
@@ -45,9 +47,27 @@ def create():
         if not title:
             flash('Title is required!')
         else:
-            conn = get_db_connection
+            conn = get_db_connection()
             conn.execute('INSERT INTO posts (title,content) VALUES (?,?)',(title,content))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
     return render_template('create.html')
+
+@app.route('/<int:id>/edit',methods=('GET','POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash("Title is required.")
+        else:
+            conn = get_db_connection()
+            conn.execute("UPDATE posts SET title = ?, content = ? WHERE id = ?",(title,content,id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('edit.html',post=post)
