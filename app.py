06e4754d47be0adca53d102c80e1d16dec0afd0e
alexizator <akilesh0876@gmail.com>
2022-08-20 19:03:32 +0530

    Release

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from crypt import methods
-from email import contentmanager
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
